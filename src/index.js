58887582014ed13201a10c04a11aca8b59e8980f
Vitalii <vit.bondarchuk@gmail.com>
2020-11-04 23:39:29 +0200

    creator

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import WebSocket from './components/WebSocket/WebSocket.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WebSocket />
   </React.StrictMode>,
   document.getElementById('root')
 );
